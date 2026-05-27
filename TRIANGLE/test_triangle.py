554d3f33fd78a78db340ccb84335504056a44ff1
Ly Nady <24021558@vnu.edu.vn>
2026-05-27 22:17:15 +0700

    Initial commit

--- TRIANGLE/test_triangle.py
+++ TRIANGLE/test_triangle.py
@@ -3,26 +3,62 @@ from triangle import classifyTriangle
 
 class TestTriangle(unittest.TestCase):
 
-    def test_invalid_negative(self):
-        self.assertEqual(classifyTriangle(-1, 1, 1), "Invalid input")
+    # TC01
+    def test_TC01(self):
+        self.assertEqual(classifyTriangle(0, 5, 5), "Invalid input")
 
-    def test_invalid_over_100(self):
-        self.assertEqual(classifyTriangle(100, 101, 200), "Invalid input")
+    # TC02
+    def test_TC02(self):
+        self.assertEqual(classifyTriangle(1, 5, 5), "Isosceles triangle")
 
-    def test_invalid_not_integer(self):
-        self.assertEqual(classifyTriangle(-1.1, 1.3, 1), "Invalid input")
+    # TC03
+    def test_TC03(self):
+        self.assertEqual(classifyTriangle(101, 5, 5), "Invalid input")
 
-    def test_not_a_triangle(self):
-        self.assertEqual(classifyTriangle(1, 2, 1), "Not a triangle")
+    # TC04
+    def test_TC04(self):
+        self.assertEqual(classifyTriangle(100, 100, 100), "Equilateral triangle")
 
-    def test_Equilateral_triangle(self):
+    # TC05
+    def test_TC05(self):
+        self.assertEqual(classifyTriangle(-1, 5, 5), "Invalid input")
+
+    # TC06
+    def test_TC06(self):
+        self.assertEqual(classifyTriangle(5.5, 5, 5), "Invalid input")
+
+    # TC07
+    def test_TC07(self):
+        self.assertEqual(classifyTriangle(1, 2, 3), "Not a triangle")
+
+    # TC08
+    def test_TC08(self):
+        self.assertEqual(classifyTriangle(2, 2, 5), "Not a triangle")
+
+    # TC09
+    def test_TC09(self):
+        self.assertEqual(classifyTriangle(10, 1, 1), "Not a triangle")
+
+    # TC10
+    def test_TC10(self):
         self.assertEqual(classifyTriangle(1, 1, 1), "Equilateral triangle")
-    
-    def test_Isosceles_triangle(self):
-        self.assertEqual(classifyTriangle(3, 3, 4), "Isosceles triangle")
-    
-    def test_Scalene_triangle(self):
+
+    # TC11
+    def test_TC11(self):
+        self.assertEqual(classifyTriangle(100, 100, 100), "Equilateral triangle")
+
+    # TC12
+    def test_TC12(self):
+        self.assertEqual(classifyTriangle(1, 2, 3), "Not a triangle")
+
+    # TC13
+    def test_TC13(self):
+        self.assertEqual(classifyTriangle(5, 5, 3), "Isosceles triangle")
+
+    # TC14
+    def test_TC14(self):
         self.assertEqual(classifyTriangle(3, 4, 5), "Scalene triangle")
-    
+
+
 if __name__ == "__main__":
     unittest.main()
